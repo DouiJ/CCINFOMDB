d78f7d559e532d78db3d93f59bc504100f700da6
DouiJ <141159120+DouiJ@users.noreply.github.com>
2024-11-17 13:40:11 +0800

    Add files via upload

---
+++ ref_job_title_management.java
@@ -0,0 +1,21 @@
+package libraryDB;
+
+public class ref_job_title_management {
+
+//    Reference Tables
+//   - Managed by System Management
+//   - Create a new Reference
+//   - Deactivate a Reference
+//   - Update a Reference
+//   - Delete a Reference
+//   - List Reference Records
+
+
+    public int job_id;          // VARCHAR(1) numbers 1 - 4
+    public String job_title;    // VARCHAR(20) based on id - 1 - Manager, 2 - Archivist, 3 - Clerk, 4 - Tech Support
+    public float salary;        // DECIMAL(6,2)
+
+
+
+
+}
