fade72b534e6d2457db26c3e93a8fee8d57f98b4
liluzivert1234 <pulvertvaldez@gmail.com>
2024-11-18 18:32:35 +0800

    Update book_inventory_management.java

--- libraryDB/book_inventory_management.java
+++ libraryDB/book_inventory_management.java
@@ -4,9 +4,18 @@ import java.sql.*;
 
 public class book_inventory_management {
 
-    public String inventory_id;  // VARCHAR(10)
-    public String isbn;             // INT
-    public String branch_id;     // VARCHAR(13)
+    public String inventory_id;
+    public String isbn;
+    public String branch_id;
+
+    public String title;
+    public String genre;
+    public Double price;
+    public String author_last_name;
+    public String author_first_name;
+
+
+
 
     public book_inventory_management() {
         inventory_id = "";
@@ -14,39 +23,47 @@ public class book_inventory_management {
         branch_id = "";
     }
 
-    //        1. Core Data Tables
-//                - Managed with Records Management
-//                - Create a new Record
-//                - Update an Existing Record
-//                - Delete an Existing Record
-//                - Deactivate an Existing Record
-//                - View one Record
-//        - List Records using a Filter
-
-    // Create a new Record
     public int add_Book() {
         try {
-
             Connection conn;
-            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/library?useTimezone=true&serverTimezone=UTC&user=root&password=3d6%vQmT");
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/library", "root", "pulvert05");
             System.out.println("Connection to DB Successful.");
 
-            Statement stmt = conn.createStatement();
-            ResultSet rs = stmt.executeQuery("SELECT MAX(inventory_id) FROM Books_Inventory");
 
-            String maxInv = "";
-            while(rs.next())
-                maxInv = rs.getString(1);
+            inventory_id = "0000000001";
 
-            int invNumber = Integer.parseInt(maxInv.substring(1)) + 1;
-            String inventory_id = String.format("%010d", invNumber);
-
-            // Inserting new record
-            PreparedStatement pstmt = conn.prepareStatement("INSERT INTO Books_Inventory (inventory_id, isbn, branch_id) VALUES (?, ?, ?)");
+            PreparedStatement pstmt = conn.prepareStatement("INSERT INTO books_inventory (inventory_id, isbn, branch_id) VALUES (?, ?, ?)");
 
             pstmt.setString(1, inventory_id);        // Automated, starts at "E0001" parse to add 1 per record
             pstmt.setString(2, isbn);
             pstmt.setString(3, branch_id);
+
+            pstmt.executeUpdate();
+            System.out.println("Record was created");
+
+            pstmt.close();
+            conn.close();
+            return 1;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return 0;
+        }
+    }
+
+    public int add_ISBN() {
+        try {
+            Connection conn;
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/library", "root", "pulvert05");
+            System.out.println("Connection to DB Successful.");
+
+            PreparedStatement pstmt = conn.prepareStatement("INSERT INTO book_details (isbn, title, genre, price, author_last_name, author_first_name) VALUES (?, ?, ?, ?, ?, ?)");
+
+            pstmt.setString(1, isbn);
+            pstmt.setString(2, title);
+            pstmt.setString(3, genre);
+            pstmt.setDouble(4, price);
+            pstmt.setString(5, author_last_name);
+            pstmt.setString(6, author_first_name);
 
             pstmt.executeUpdate();
             System.out.println("Record was created");
@@ -87,16 +104,41 @@ public class book_inventory_management {
     public int update_ISBN() {
         try {
             Connection conn;
-            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/library?useTimezone=true&serverTimezone=UTC&user=root&password=3d6%vQmT");
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/library", "root", "pulvert05");
             System.out.println("Connection to DB Successful.");
 
             PreparedStatement pstmt = conn.prepareStatement("UPDATE Book_Details SET title=?, genre=?, price=?, author_last_name=?, author_first_name=? WHERE isbn=?");
 
-            pstmt.setString(1, isbn);
-            pstmt.setString(2, branch_id);;
+            pstmt.setString(1, title);
+            pstmt.setString(2, genre);
+            pstmt.setDouble(3, price);
+            pstmt.setString(4, author_last_name);
+            pstmt.setString(5, author_first_name);
+            pstmt.setString(6, isbn);
 
             pstmt.executeUpdate();
             System.out.println("Record was updated");
+
+            pstmt.close();
+            conn.close();
+            return 1;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return 0;
+        }
+    }
+
+    public int delete_ISBN() {
+        try {
+            Connection conn;
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/library", "root", "pulvert05");
+            System.out.println("Connection to DB Successful.");
+
+            PreparedStatement pstmt = conn.prepareStatement("DELETE FROM book_details WHERE isbn=?");
+            pstmt.setString(1, isbn);
+
+            pstmt.executeUpdate();
+            System.out.println("Record was deleted");
 
             pstmt.close();
             conn.close();
@@ -123,6 +165,34 @@ public class book_inventory_management {
             pstmt.close();
             conn.close();
             return 1;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return 0;
+        }
+    }
+
+    public int get_ISBN() {
+        int recordcount = 0;
+
+        try {
+            Connection conn;
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/library", "root", "pulvert05");
+            System.out.println("Connection to DB Successful.");
+
+            PreparedStatement pstmt = conn.prepareStatement("SELECT * FROM book_details WHERE isbn=?");
+            pstmt.setString(1, isbn);
+            ResultSet rs = pstmt.executeQuery();
+
+            while (rs.next()) {
+                title = rs.getString("title");
+                genre = rs.getString("genre");
+                price = rs.getDouble("price");
+                author_last_name = rs.getString("author_last_name");
+                author_first_name = rs.getString("author_first_name");
+                recordcount++;
+            }
+
+            return recordcount;
         } catch (Exception e) {
             System.out.println(e.getMessage());
             return 0;
